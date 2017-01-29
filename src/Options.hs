5c0fc7bae1a45e62cff6739d1aa9d8b4bea93af9
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-01-29 04:25:10 +0000

    hackish fix of the option parsing

--- src/Options.hs
+++ src/Options.hs
@@ -1,22 +1,31 @@
 module Options
-( Options
-, ShowOptions
-, InfoOptions
+( Options(..)
 , parser
+, perform
 ) where
 
+import Options.Applicative
 import qualified Command.Show.Options as CShow
+import qualified Command.Show.Perform as CShow
 import qualified Command.Info.Options as CInfo
+import qualified Command.Info.Perform as CInfo
 
+
+-- | Command-line subcommands.
 data Command
-  = CmdShow ShowOptions
-  | CmdInfo InfoOptions
+  = CmdShow CShow.ShowOptions
+  | CmdInfo CInfo.InfoOptions
+  deriving (Show)
 
+-- | Command-line options.
 data Options = Options { optCommand :: Command }
+  deriving (Show)
 
+-- | Parsing of command-line subcommands.
+commands :: Parser Command
 commands = subparser
-  $ command "show" 
- <> command "info" 
+  $ command "show" (info (fmap CmdShow CShow.showOptDefine) fullDesc)
+ <> command "info" (info (fmap CmdInfo CInfo.infoOptDefine) fullDesc)
 -- <> command "compress"
 -- <> command "decompress"
 -- <> command "check"
@@ -25,19 +34,19 @@ commands = subparser
 -- <> command "export-csv"
 
 optionsParser :: Parser Options -- ^ parser
-optionsParser = Options <$> optionCommand
+optionsParser = Options <$> commands --optionCommand
 
 -- | Description of the utility.
 optionsDesc :: InfoMod Options -- ^ parser description
 optionsDesc = headerDesc <> fullDesc
-  where headerDesc = header "GoatSwim - time series database combinators"
+  where headerDesc = header "swim - time series database combinators"
 
 -- | Parser of the command-line options.
 parser :: ParserInfo Options -- ^ parser
 parser = info (helper <*> optionsParser) optionsDesc
 
 -- | Perform a selected command.
-performCommand :: Command -- ^ command
-               -> IO ()   -- ^ action
-performCommand (CmdShow opt) = CShow.perform opt
-performCommand (CmdInfo opt) = CInfo.perform opt
+perform :: Command -- ^ command
+        -> IO ()   -- ^ action
+perform (CmdShow opt) = CShow.perform opt
+perform (CmdInfo opt) = CInfo.perform opt
