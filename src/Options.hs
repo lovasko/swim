9a40966d7513281bcfaeadee296543bd0a911b39
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-02-11 13:19:37 +0000

    support for round and compress commans

--- src/Options.hs
+++ src/Options.hs
@@ -9,12 +9,16 @@ import qualified Command.Show.Options as CShow
 import qualified Command.Show.Perform as CShow
 import qualified Command.Info.Options as CInfo
 import qualified Command.Info.Perform as CInfo
+import qualified Command.Round.Perform as CRound
+import qualified Command.Compress.Perform as CCompress
 
 
 -- | Command-line subcommands.
 data Command
-  = CmdShow CShow.ShowOptions
-  | CmdInfo CInfo.InfoOptions
+  = CmdShow  CShow.ShowOptions
+  | CmdInfo  CInfo.InfoOptions
+  | CmdRound CRound.RoundOptions
+  | CmdCompress CCompress.CompressOptions
   deriving (Show)
 
 -- | Command-line options.
@@ -26,7 +30,8 @@ commands :: Parser Command
 commands = subparser
   $ command "show" (info (fmap CmdShow CShow.showOptDefine) fullDesc)
  <> command "info" (info (fmap CmdInfo CInfo.infoOptDefine) fullDesc)
--- <> command "compress"
+ <> command "round" (info (fmap CmdRound CRound.options) fullDesc)
+ <> command "compress" (info (fmap CmdCompress CCompress.options) fullDesc)
 -- <> command "decompress"
 -- <> command "check"
 -- <> command "shift"
@@ -50,3 +55,5 @@ perform :: Command -- ^ command
         -> IO ()   -- ^ action
 perform (CmdShow opt) = CShow.perform opt
 perform (CmdInfo opt) = CInfo.perform opt
+perform (CmdRound opt) = CRound.perform opt
+perform (CmdCompress opt) = CCompress.perform opt
