0dea2bb8464cf0d16e3f0ad7e91f726593db50bf
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-02-11 13:17:31 +0000

    lambda case; options unification

--- src/Command/Compress/Perform.hs
+++ src/Command/Compress/Perform.hs
@@ -1,34 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Command.Zip.Perform
-( perform
+module Command.Compress.Perform
+( CompressOptions
+, perform
+, options
 ) where
 
 import Data.Monoid
+import Options.Applicative
 import System.Exit
-import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-import Command.Zip.Options
 import Load
 import Save
-import Variant
+import Format
 
+
+-- | Command-line arguments.
+data CompressOptions = CompressOptions { compressFile :: FilePath }
+  deriving (Show)
+
+-- | Data file to compress.
+optFile :: Parser FilePath -- ^ parser
+optFile = argument str (metavar "FILE")
+
+-- | Command-line user interface.
+options :: Parser CompressOptions -- ^ parser
+options = CompressOptions <$> optFile
 
 -- | Load a story and save it right back compressed.
 compress :: FilePath -- ^ file path
          -> IO ()    -- ^ action
-compress path = do
-  result <- storyLoad path
-  case result of
-    Left  err   -> T.putStrLn ("ERROR: " <> err) >> exitFailure
-    Right story -> storySave story path          >> exitSuccess
+compress path = storyLoad path >>= \case
+  Left  err   -> T.putStrLn ("ERROR: " <> err)           >> exitFailure
+  Right story -> storySave story (fmtChange FmtZip path) >> exitSuccess
 
 -- | Peform story compression on a selected file.
-perform :: ZipOptions -- ^ compress options
-        -> IO ()      -- ^ command action
-perform options = case varIdentify path of
+perform :: CompressOptions -- ^ compress options
+        -> IO ()           -- ^ command action
+perform opts = case fmtIdentify path of
   Left err     -> T.putStrLn ("ERROR: " <> err)          >> exitFailure
-  Right VarZip -> T.putStrLn "ERROR: Already compressed" >> exitFailure
-  Right VarRaw -> compress path
-  where path = zipOptFile options
+  Right FmtZip -> T.putStrLn "ERROR: Already compressed" >> exitFailure
+  Right FmtRaw -> compress path
+  where path = compressFile opts
